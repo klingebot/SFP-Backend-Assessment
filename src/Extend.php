a3461b0039b2dd2c078c36ff402855e075799c13
klingebot <alexriley1223@gmail.com>
2021-02-20 20:06:25 -0500

    Finished Extend class and logic

--- src/Extend.php
+++ src/Extend.php
@@ -1,9 +1,13 @@
 <?php namespace Sfp;
   require_once './Rotate.php';
   class Extend extends Rotate {
-    public function execute(){
-
+    public function execute($amount){
+      $rotated = Rotate::execute($amount);
+      return end($rotated);
     }
   }
 
+//Debugging
+$extend = new Extend();
+print_r($extend->execute("3"));
  ?>
