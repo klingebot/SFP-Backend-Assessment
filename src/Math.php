2b6bae644e563400f233233f500384d05e08b744
klingebot <alexriley1223@gmail.com>
2021-02-20 20:23:21 -0500

    Clean up and finish comments

--- src/Math.php
+++ src/Math.php
@@ -1,11 +1,19 @@
 <?php namespace Sfp;
   class Math {
+    /**
+     * From a given .csv file, find the average of all values whose value=TRUE
+     *
+     * @return Double Average amount of all values that are true in dataset
+     *
+     */
     public function execute(){
-      //Preliminary variables
-      //
-      // count: # of valid entries found
-      // total: running total from valid entries
-      //
+      /**
+       * Variables to be used in averaging math
+       *
+       * @var integer $count Number of valid entries found in dataset matching params
+       * @var integer $total Running total from valid entries
+       *
+       */
       $count = 0;
       $total = 0;
 
@@ -14,13 +22,15 @@
 
       //Start at 1 to skip over csv formatting key,value,accept
       for($x = 1; $x < count($csv); $x++){
+
+        //If valid entry is found, increase count by 1 and add amount to running total
         if($csv[$x][2] == "true"){
           $count++;
           $total += $csv[$x][1];
         }
       }
 
-      //Account for empty csv file/no valid entries (non-applicable)
+      //Account for empty csv file/no valid entries (non-applicable but good practice)
       if($count != 0){
         return $total/$count;
       }else{
@@ -28,8 +38,4 @@
       }
     }
   }
-
-//Debugging
-$mathStuff = new Math();
-print_r($mathStuff->execute());
  ?>
