84c7935b2197ba90a4768b9a60ad88fa82152ffa
klingebot <alexriley1223@gmail.com>
2021-02-20 20:16:43 -0500

    Clean up and add final comments

--- src/Extend.php
+++ src/Extend.php
@@ -1,13 +1,21 @@
 <?php namespace Sfp;
+  // Import Rotate file/class
   require_once './Rotate.php';
+
   class Extend extends Rotate {
+    /**
+     * Returns the last value from an executed Rotate parent function
+     * 
+     * @param  integer $amount Number of times to rotate array (left)
+     * @return string         Last item in rotated array
+     *
+     */
     public function execute($amount){
+      //Execute parent function and store returned array in $rotated
       $rotated = Rotate::execute($amount);
+
+      //Get last item in array and return as string
       return end($rotated);
     }
   }
-
-//Debugging
-$extend = new Extend();
-print_r($extend->execute("3"));
  ?>
